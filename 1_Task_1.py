165584c9ded8f297a6e8580f8da9aa33d8b379bb
Olga <zybastik1308@mail.ru>
2021-10-26 17:16:50 +0300

    Выполнение первого практического задания

---
+++ 1_Task_1.py
@@ -0,0 +1,23 @@
+import pandas
+import numpy
+
+# TODO - 1. Поработайте с переменными, создайте несколько, выведите на экран,
+# запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
+
+vrl_1 = 'Varibles int ='
+vrl_2 = "Varibles float"
+vrl_3 = 2.0
+vrl_4 = -4
+vrl_5 = [431, -0.23, 'Text']
+
+print(vrl_1, vrl_4)
+print(vrl_2, '=', vrl_3)
+print(vrl_5)
+
+apart = int(input("Какой у вас номер дома?"))
+street = input("Какая у вас улица?")
+print(f'Ваш адрес: улица {street} д. {apart}')
+
+
+
+
